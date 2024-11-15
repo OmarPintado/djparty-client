a801ea12c0b7dba9afaa2b1720207e78086ea7e7
Jamila Isabel Palacios Miguel <70769302+JamilaP@users.noreply.github.com>
2024-11-15 14:21:44 -0500

    Update access to a details room

--- src/components/room/RoomListRow.tsx
+++ src/components/room/RoomListRow.tsx
@@ -1,35 +1,43 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import CardItem from '../common/Card/CardItem';
 import './css/RoomListRow.css';
 
-
 interface Room {
-    image: string;
-    title: string;
-    subtitle: string;
-    options: { label: string; action: () => void }[];
+  id: string; 
+  image: string;
+  title: string;
+  subtitle: string;
+  options: { label: string; action: () => void }[];
 }
 
-interface ListRoomRowProps {
-    rooms: Room[];
+interface RoomListRowProps {
+  rooms: Room[];
 }
 
-const RoomListRow: React.FC<ListRoomRowProps> = ({ rooms }) => {
-    return (
-        <div className="room-list-row">
-            {rooms.map((room, index) => (
-                <CardItem
-                    key={index}
-                    image={room.image}
-                    title={room.title}
-                    subtitle={room.subtitle}
-                    options={[]}
-                    showAddButton={false}
-                    number={undefined}
-                />
-            ))}
-        </div>
-    );
+const RoomListRow: React.FC<RoomListRowProps> = ({ rooms }) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = (roomId: string) => {
+    navigate(`/room-home/${roomId}`);
+  };
+
+  return (
+    <div className="room-list-row">
+      {rooms.map((room, index) => (
+        <CardItem
+          key={index}
+          image={room.image}
+          title={room.title}
+          subtitle={room.subtitle}
+          options={room.options}
+          showAddButton={false}
+          number={undefined}
+          onClick={() => handleCardClick(room.id)} // Pasar onClick con roomId
+        />
+      ))}
+    </div>
+  );
 };
 
 export default RoomListRow;
